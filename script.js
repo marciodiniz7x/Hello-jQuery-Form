6adb06673d85ca422e5b49c855c8e89c6ef4f514
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-20 18:27:04 -0300

    Estilizei um pouco mais

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Script Seção 1) Digitação de nome em formulário.
-
-
+// Carregamento inicial
 $(document).ready(function() {
+
+    // Script Seção 1) Digitação de nome em formulário.
     $("#aqui").click(function() {
 
         let nome = $('#nome').val();
@@ -17,28 +17,29 @@ $(document).ready(function() {
             saudacao.html(`Olá, <strong>${nome}</strong>. Seu nome foi validado e será utilizado como referência em todo este ambiente.`);
         }
 
-        // Script Seção 2) Interação com botão em AJAX.
-        $('#mais').click(function() {
-
-            $('#mais').remove();
-            $('.info').css("width", "500px")
+    })
     
-            $.ajax({url:"conteudo.txt", success: function(resultado) {
-                $('.info').html(resultado);
-            }});
-           
-        });
+    // Script Seção 2) Interação com botão em AJAX.
+    $('#mais').click(function() {
 
-        // Seção 3) Manipulando números com jQuery.
-        $('.calcular').click(function() {
+        $('#mais').remove();
+        $('.info').css("width", "500px")
 
-            let valor1 = Number($('.valor1').val());
-            let valor2 = Number($('.valor2').val());
-            let media = (valor1 + valor2) / 2;
-            let res = $('.res')
-            
-            res.html(`<strong>${nome}</strong>, nosso sistema calculou a média entre <strong><span style="color: #f52af5;">${valor1}</span></strong> e <strong><span style="color: #f52af5;">${valor2}</span></strong>. <br> E o resultado foi: <strong> <span style="color: #f52af5;">${media}</span></strong>`)
-        })
+        $.ajax({url:"conteudo.txt", success: function(resultado) {
+            $('.info').html(resultado);
+        }});
+        
+    });
 
+    // Seção 3) Manipulando números com jQuery.
+    $('.calcular').click(function() {
+
+        let nome = $('#nome').val();
+        let valor1 = Number($('.valor1').val());
+        let valor2 = Number($('.valor2').val());
+        let media = (valor1 + valor2) / 2;
+        let res = $('.res')
+        
+        res.html(`<strong>${nome}</strong>, nosso sistema calculou a média entre <strong><span style="color: #f52af5;">${valor1}</span></strong> e <strong><span style="color: #f52af5;">${valor2}</span></strong>. <br> E o resultado foi: <strong> <span style="color: #f52af5;">${media}</span></strong>`)
     })
 });
